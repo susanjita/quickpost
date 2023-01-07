7c92f33255bbc9baf0a2fdd3e9b1c38979a686f2
susanjita <susanjitamall.1987@gmail.com>
2023-01-07 10:36:14 -0800

    initial commmit

---
+++ src/main/java/com/quickpost/quickpost/QuickpostApplication.java
@@ -0,0 +1,13 @@
+package com.quickpost.quickpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickpostApplication.class, args);
+	}
+
+}
